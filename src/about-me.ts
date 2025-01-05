56483ea5868585ba425d9ce6582906062fd4a46b
Aruzhan Pulat <118962342+Aruzhan0504@users.noreply.github.com>
2025-01-05 19:47:27 +0500

    Update about-me.ts

--- src/about-me.ts
+++ src/about-me.ts
@@ -1,6 +1,6 @@
 export const contactInfo = {
-    name: 'Имя Фамилия', //Укажите здесь имя
-    phoneNumber: '+77713011111', //Укажите здесь свой номер телефона
-    email: 'yourmail@gmail.com', //Укажите здесь свой email
-    bio: '' //Напишите сопроводительное письмо(от 50 символов!!!)
+    name: 'Aruzhan Pulat', //Укажите здесь имя
+    phoneNumber: '+77072895187', //Укажите здесь свой номер телефона
+    email: 'aruzhanp2004@gmail.com', //Укажите здесь свой email
+    bio: 'I'm 20 years old.I'm styding on 4th course in university IITU.I learn and know Java,PostgreSQL,Docker,GitLab,Android Studio,Xcode.In the time i want to open the wold IT. ' //Напишите сопроводительное письмо(от 50 символов!!!)
 }  
